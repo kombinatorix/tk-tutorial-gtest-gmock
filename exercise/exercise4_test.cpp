a68d4f8a00154f4edc84084697250dcc2dcb02c2
Sebastian Schönitz <sebastian.schoenitz@ima-zlw-ifu.rwth-aachen.de>
2017-01-17 16:53:06 +0100

    Added exercise 4 with soultion

---
+++ exercise/exercise4_test.cpp
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+
+TEST(Case1, Test1){
+	SUCCEED();
+}
+
+TEST(Case1, Test2){
+	SUCCEED();
+}
+TEST(Case1, Test3){
+	FAIL();
+}
+TEST(Case1, Test4){
+	SUCCEED();
+}
+
+
+
+TEST(Case2, Test1FAIL){
+	FAIL();
+}
+TEST(Case2, Test2SUCCEED){
+	SUCCEED();
+}
+TEST(Case2, Test3FAIL){
+	FAIL();
+}
+TEST(Case2, Test4FAIL){
+	FAIL();
+}
+TEST(Case2, Test5SUCCEED){
+	SUCCEED();
+}
+
+
+
+TEST(TOTALFAIL, Test1){
+	FAIL();
+}
+TEST(TOTALFAIL, Test2){
+	FAIL();
+}
+TEST(TOTALFAIL, Test3){
+	FAIL();
+}
+
+int main(int argc,char **argv){
+
+	// Hier den Filter einfügen.
+	
+	::testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
