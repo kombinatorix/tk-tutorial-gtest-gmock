5aae5e3d00f94e5a89268954df2435b16879b25e
Sebastian Schönitz <sebastian.schoenitz@ima-zlw-ifu.rwth-aachen.de>
2017-01-19 12:55:06 +0100

    Added missing exercise3_solution.cpp

---
+++ exercise/exercise3_solution.cpp
@@ -0,0 +1,8 @@
+#include "include/exercise3_solution.hpp"
+
+std::string StringCombination::concatenate1(){
+	return this->foo+this->bar;
+}
+std::string StringCombination::concatenate2(){
+	return this->bar+this->foo;
+}
